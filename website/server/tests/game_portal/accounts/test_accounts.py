f8687e5b36b75554b3f16a1517b1e586aae973ee
Chris Wu <christopherjwu2000@gmail.com>
2023-05-23 15:28:23 -0400

    added test comment to test_accounts.py

--- website/server/tests/game_portal/accounts/test_accounts.py
+++ website/server/tests/game_portal/accounts/test_accounts.py
@@ -66,6 +66,7 @@ class TestAccounts:
             "email": "myname-at-email.com",
             "password": "validPassword",
         }
+        #comment test
 
         response = api_client.post(self.create_url, data, format="json")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
